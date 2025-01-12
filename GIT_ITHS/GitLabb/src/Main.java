8d39d1702eb3ca0dccfe309c12d373c5071f10dc
RobertMili <robertmilicevic3869@gmail.com>
2025-01-12 21:00:04 +0100

    add GITHUB

---
+++ GIT_ITHS/GitLabb/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println(test(2));
+    }
+    public static int test (int a){
+        return a;
+    }
+
+}
